182cde98a966bef8b2fdb4024e957e146e68db3f
sayin47 <sayinmehmet47@gmail.com>
2021-09-28 01:42:13 +0300

    server edited

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({ static: './build' });
 const port = process.env.PORT || 8000;
+const cors = require('cors');
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+server.use(cors(corsOptions));
 
 server.use(middlewares);
 server.use(router);
