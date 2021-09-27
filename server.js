c8bcfc0a05df520977fae3fc00e58dacb36d7633
sayin47 <sayinmehmet47@gmail.com>
2021-09-27 21:53:38 +0300

    json file changed

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const jsonServer = require('json-server');
-const app = jsonServer.create();
-const path = require('path');
 const server = jsonServer.create();
-
-const express = require('express');
-const middlewares = jsonServer.defaults();
 const router = jsonServer.router('db.json');
-const port = process.env.PORT || 3001;
+const middlewares = jsonServer.defaults({ static: './build' });
+const port = process.env.PORT || 3000;
 
-app.use('/db', middlewares, router);
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+server.use(middlewares);
+server.use(router);
 
 server.listen(port);
